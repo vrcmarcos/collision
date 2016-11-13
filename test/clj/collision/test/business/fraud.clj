4619708ecbad2ec4aff47d8a9a5566fee99046aa
Marcos Cardoso <vrcmarcos@gmail.com>
2016-11-13 14:28:03 -0200

    Testing the PROBLEM.md example

--- test/clj/collision/test/business/fraud.clj
+++ test/clj/collision/test/business/fraud.clj
@@ -6,4 +6,15 @@
   (testing "should return [[1 2]] as networks when empty network is given and add collision [1 2]"
     (is (= [[1 2]] (add-collision [] [1 2]))))
   (testing "should return [[1 2 3]] as networks when networks are [[1 2]] and add collision [2 3]"
-    (is (= [[1 2 3]] (add-collision [[1 2]] [2 3])))))
+    (is (= [[1 2 3]] (add-collision [[1 2]] [2 3]))))
+  (testing "should return [[1 2 3 4] [5 6 7]] when adding collisions [1, 2] [2, 3] [1, 4] [5, 6] [6, 7] to empty network"
+    (is (= [[1 2 3 4] [5 6 7]]
+           (add-collision
+             (add-collision
+               (add-collision
+                 (add-collision
+                   (add-collision [] [1 2])
+                   [2 3])
+                 [1 4])
+               [5 6])
+             [6 7])))))
